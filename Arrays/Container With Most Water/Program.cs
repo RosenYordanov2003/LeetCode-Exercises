5730116feec180c3179399d5346cc295898dac58
Rosen Yordanov <roskata182003@gmail.com>
2023-01-24 13:13:31 +0200

    Upload 2 solved problems

---
+++ Arrays/Container With Most Water/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace Container_With_Most_Water
+{
+    //area min(element1,element2)*posDiff
+    //55 tests from 61 test were passed
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[9] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+            int result = MaxArea(array);
+            Console.WriteLine(result);
+        }
+        public static int MaxArea(int[] height)
+        {
+            int leftSide = 0,
+            rightSide = height.Length - 1,
+            currentArea = 0,
+            max = int.MinValue;
+            while (leftSide < rightSide)
+            {
+                currentArea = (rightSide - leftSide) * Math.Min(height[leftSide], height[rightSide]);
+                if (currentArea > max)
+                {
+                    max = currentArea;
+                }
+
+                if (height[leftSide] <= height[rightSide])
+                {
+                    leftSide++;
+                }
+                else
+                {
+                    rightSide--;
+                }
+            }
+            return max;
+
+        }
+    }
+}
---
+++ Arrays/Three Sum/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Three_Sum
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        private static IList<IList<int>> ThreeSum(int[] nums)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    for (int k = i + 2; k < nums.Length; k++)
+                    {
+                        if (nums[i] + nums[j] == 0 && nums[k]>0)
+                        {
+                            continue;
+                        }
+                        if (i != j && i != k && j != k)
+                        {
+                            if (nums[i] + nums[j] + nums[k] == 0)
+                            {
+                                List<int> currentCollection = new List<int>() { nums[i], nums[j], nums[k] };
+                                if (!CheckForDuplicate(result, currentCollection))
+                                {
+                                    result.Add(new List<int> { nums[i], nums[j], nums[k] });
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        private static bool CheckForDuplicate(IList<IList<int>> result, List<int> currentCollection) =>
+             result.Any(x => x.All(currentCollection.Contains) && currentCollection.All(x.Contains));
+    }
+}
+
+
+
